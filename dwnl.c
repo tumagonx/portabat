a4b1ced287ec9f5b0176dd3e075aa6abadc45254
tumagonx <mr.tiar@gmail.com>
2017-07-26 13:23:57 +0700

    force unicode

--- dwnl.c
+++ dwnl.c
@@ -1,3 +1,24 @@
+/*
+ * ReactOS
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
+ */
+#ifndef UNICODE
+#define UNICODE
+#endif
+#define _UNICODE
 #define COBJMACROS
 #include <urlmon.h>
 #include <wininet.h>
@@ -11,7 +32,6 @@ typedef const wchar_t *STRSAFE_LPCWSTR;
 STRSAFEAPI StringVPrintfWorkerW(STRSAFE_LPWSTR pszDest,size_t cchDest,STRSAFE_LPCWSTR pszFormat,va_list argList);
 STRSAFEAPI StringCbVPrintfW(STRSAFE_LPWSTR pszDest,size_t cbDest,STRSAFE_LPCWSTR pszFormat,va_list argList);
 
-/* FIXME: add correct definitions to urlmon.idl */
 #ifdef UNICODE
 #define StringCbVPrintf StringCbVPrintfW
 #else
