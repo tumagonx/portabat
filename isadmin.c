37c7bba506e9c5ab0a174798d0966a52297df4e9
tumagonx <mr.tiar@gmail.com>
2017-09-18 09:14:30 +0700

    Rewrite isadmin as advised by MSDN

--- isadmin.c
+++ isadmin.c
@@ -1,17 +1,46 @@
-// public domain
+// See MSDN CheckTokenMembership function example
 #include <stdio.h>
 #include <windows.h>
 
 #ifdef __TINYC__
-BOOL IsUserAnAdmin(void);
-#else
-#include <shlobj.h>
+DECLSPEC_IMPORT WINBOOL APIENTRY CheckTokenMembership (HANDLE , PSID , PBOOL );
+//BOOL IsUserAnAdmin(void);
 #endif
+
+#pragma comment(lib, "advapi32.lib")
+
 int main(void) {
-    if (IsUserAnAdmin()) {
-            printf("1\n");
-            return 0;
-    }
-    printf("0\n");
-    return 1;
+/*++ 
+Routine Description: This routine returns TRUE if the caller's
+process is a member of the Administrators local group. Caller is NOT
+expected to be impersonating anyone and is expected to be able to
+open its own process and process token. 
+Arguments: None. 
+Return Value: 
+   TRUE - Caller has Administrators local group. 
+   FALSE - Caller does not have Administrators local group. --
+*/ 
+BOOL b;
+SID_IDENTIFIER_AUTHORITY NtAuthority = SECURITY_NT_AUTHORITY;
+PSID AdministratorsGroup; 
+b = AllocateAndInitializeSid(
+    &NtAuthority,
+    2,
+    SECURITY_BUILTIN_DOMAIN_RID,
+    DOMAIN_ALIAS_RID_ADMINS,
+    0, 0, 0, 0, 0, 0,
+    &AdministratorsGroup); 
+if(b) 
+{
+    if (!CheckTokenMembership( NULL, AdministratorsGroup, &b)) 
+    {
+         b = FALSE;
+    } 
+    FreeSid(AdministratorsGroup); 
 }
+printf("%d\n", b);
+if(b) return 0;
+else return 1;
+}
+
+
